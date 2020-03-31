ca9d7d8fe19f99892815b1476cd60186de9ed573
Julian Solorzano <yayforme789@gmail.com>
2020-03-31 13:49:09 -0600

    client.py

---
+++ client.py
@@ -0,0 +1,26 @@
+import requests
+
+class FlareClient():
+    def __init__(self, host="http://localhost:6800"):
+        self.host = host
+        self.headers = {}
+
+    def login(self, login, password, email=False):
+        resp = requests.post(self.host + "/auth", json={
+            ('login' if not email else 'email'): login,
+            'password': password,
+        }).json()
+        if "result" in resp:
+            print("Login succesful")
+            self.headers = {'Authorization': 'Bearer ' + resp["result"]}
+        else:
+            print(resp)
+
+    def call(self, model, method, *args, **kwargs):
+        resp = requests.post(self.host + "/call", headers=self.headers, json={
+            'model': model,
+            'method': method,
+            'args': args,
+            'kwargs': kwargs
+        })
+        return resp.json()

6db92fd22b59044c19a2ffdfd1d75f100b8fefbc
EvasTripi <a.morales.stats@gmail.com>
2020-11-19 19:38:47 -0600

    Update svelte files

--- svelte_client/services/menu.js
+++ svelte_client/services/menu.js
@@ -4,7 +4,15 @@ export function getMenus() {
     return call("FlrMenu", "get_menus");
 }
 
-export function getViews(menuId) {
-    return call("FlrView", "read", [["model","definition","view_type"]],
-        {filters: [['menu_id','=',menuId]], order: "sequence"})
+export function getViews(type) {
+    let kwargs = {}
+    if (typeof type === 'number'){
+        kwargs.filters = [['menu_id','=',type]]
+        kwargs.order = "sequence"
+    }
+    if (typeof type === 'string'){
+        kwargs.filters = [['model','=',type]]
+    }
+    return call("FlrView", "read", [
+        ["model","definition","view_type","menu_id","name"]], kwargs)
 }

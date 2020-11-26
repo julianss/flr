dd803fb77f20ab06db058baa1f06e6285b3f52ee
EvasTripi <a.morales.stats@gmail.com>
2020-11-25 20:46:56 -0600

    Add new fields to model FlrPreferences for change the password

--- core_models/user.py
+++ core_models/user.py
@@ -141,8 +141,28 @@ FlrUser.r()
 FlrGroup.r()
 
 class FlrPreferences(BaseModel):
-    old_password = pw.CharField(help_text="Password")
-    new_password = pw.CharField(help_text="Password")
+    user = pw.ForeignKeyField(FlrUser, help_text="Usuario")
+    old_password = pw.CharField(help_text="Password anterior")
+    new_password = pw.CharField(help_text="Password nuevo")
+    confirm_new_password = pw.CharField(help_text="Password confirmación")
+
+    @classmethod
+    def get_default(cls):
+        user = FlrUser.get_by_id(request.uid)
+        return {
+            'user': user.get_dict_id_and_name(),
+        }
+
+    @classmethod
+    def flr_create(cls, **fields):
+        user = FlrUser.get_by_id(fields.get("user_id"))
+        if not pbkdf2_sha512.verify(fields.get("old_password"), user.password):
+            raise Exception("El password anterior no coincide")
+        elif not fields.get('new_password') == fields.get('confirm_new_password'):
+            raise Exception("El password nuevo y el password de confirmación no son identicos")
+        FlrUser.flr_update({'password': fields.get('new_password')},
+            [('id','=',user.id)])
+        return super(FlrPreferences, cls).flr_create(**fields)
 
 FlrPreferences.r()
 

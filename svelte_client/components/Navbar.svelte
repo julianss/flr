ae2fc142ad851545966e6149d32b281a886d2428
Julian Solorzano <yayforme789@gmail.com>
2021-02-04 13:08:55 -0600

    Remove style width from the navbar logo

--- svelte_client/components/Navbar.svelte
+++ svelte_client/components/Navbar.svelte
@@ -63,7 +63,7 @@
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
   <span class="navbar-brand">
-    <img src="images/logo_navbar.png" style="width:45px" alt="Logo"/>
+    <img src="images/logo_navbar.png" alt="Logo"/>
   </span>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
